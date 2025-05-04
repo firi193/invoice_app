bdb434a580cf6570d5c84c61bd1507058758d8c1
Firi <56671623+firi193@users.noreply.github.com>
2025-05-05 00:31:59 +0300

    Update ingest.py

--- app/pipeline/ingest.py
+++ app/pipeline/ingest.py
@@ -5,5 +5,5 @@ class Ingestor:
         self.engine = create_engine(db_url)
 
     def insert(self, invoices_df, line_items_df):
-        invoices_df.to_sql("invoices", self.engine, if_exists="replace", index=False)
-        line_items_df.to_sql("line_items", self.engine, if_exists="replace", index=False)
+        invoices_df.to_sql("invoices", self.engine, if_exists="append", index=False)
+        line_items_df.to_sql("line_items", self.engine, if_exists="append", index=False)
